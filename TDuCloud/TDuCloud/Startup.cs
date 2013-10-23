01f2b707eb39bfa530f3040f4c8b60bac4d327af
swxu <swxu@xmtdu.com>
2013-10-23 18:01:26 +0800

    饿35

---
+++ TDuCloud/TDuCloud/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TDuCloud.Startup))]
+namespace TDuCloud
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
